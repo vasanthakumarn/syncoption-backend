6450c4369395a08926cb86e3588c099c737077be
vasanthakumarn <41970169+vasanthakumarn@users.noreply.github.com>
2021-05-31 10:47:03 +0530

    Added Procfile for Heroku Deployment

--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,40 @@ const port = 3000;
 const app = express();
 app.use(cors({origin: '*'}));
 
-app.get('/', (req, res) => {
-    res.send('Get Request Working Fine');
+/**
+ * MARKET DATA API END POINT AND SEND RESPONSE AFTER 10SECONDS
+ */
+app.get('/marketdata', (req, res) => {
+    let data = getMarketData();
+    //SIMULATE TO CANCEL THE REQUEST FROM FRONT END
+    setTimeout(() => {
+        res.send({data});
+    }, 10000);
 });
 
+/**
+ * STATS DATA API END POINT AND SEND RESPONSE
+ */
+app.get('/statsdata', (req, res) => {
+    let data = getStatsData();
+    res.send(data);
+});
+
+/**
+ * LISTEN FOR REQUESTS
+ */
 const httpServer = app.listen(port, () => {
     console.log("SyncOption backend started in Port ", port);
 });
 
+/**
+ * CREATE SOCKET ON SERVER
+ */
 const io = new Server(httpServer, {cors: { origin: '*' }});
 
+/**
+ * GENERATE RANDOM MARKET DATA WITH SOME RANDOM VALUES
+ */
 const getMarketData = (): any => {
     return [
         {name: 'EURUSD', bid: parseFloat(1.234 * Math.random()+'').toFixed(5), ask: parseFloat(1.234 * Math.random()+'').toFixed(5), onedaychange:1.234, type: 'forex'},
@@ -35,6 +59,9 @@ const getMarketData = (): any => {
     ]
 }
 
+/**
+ * SEND MARKET DATA THROUGH SOCKET
+ */
 const sendMarketData = (client: any) => {
     setInterval(() => {
         let data = getMarketData();
@@ -42,23 +69,32 @@ const sendMarketData = (client: any) => {
     }, 1000);
 }
 
+/**
+ * CREATE STATS DATA WITH RANDOM VALUES
+ */
 const getStatsData = (): any => {
+    const minimum = 20;
+    const maxmimum = 65;
     return {
         date: new Date(),
-        cpuUtilization: Math.floor(Math.random()*100).toFixed(2);
+        cpuUtilization: Math.floor(minimum+Math.random()*maxmimum).toFixed(2)
     }
 }
 
+/**
+ * SEND STATS DATA THROUGH SOCKETS AFTER 10SECONDS
+ */
 const sendStatsData = (client: any) => {
     setInterval(() => {
         let data = getStatsData();
-        console.log(data);
         client.emit('statsChange', data);
-    }, 5000);
+    }, 10000);
 }
 
+/**
+ * AFTER CLIENT IS CONNECTED START SENDING MARKET DATA AND STATS DATA
+ */
 io.on('connection', (client) => {
-    console.log('Client Connected');
     sendMarketData(client);
     sendStatsData(client);
 });
